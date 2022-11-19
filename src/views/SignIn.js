b21e410debf199114a4ae930ea74792e2d3ba6e3
maxl3oss <maxl3oss@gmail.com>
2022-11-20 03:57:11 +0700

    ninth commit

--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -101,7 +101,7 @@ export default function SignIn() {
                         Lost Password?
                      </span>
                   </div>
-                  <button type="submit" onClick={onSubmit} className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:border-blue-500 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign In to your account</button>
+                  <button type="submit" onClick={onSubmit} className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:border-blue-500 focus:ring-blue-400 focus:outline-none focus:ring-opacity-40 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign In to your account</button>
                   <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
                      Not registered?
                      <Link to="/signUp" className="text-blue-700 hover:underline dark:text-blue-500">Create
